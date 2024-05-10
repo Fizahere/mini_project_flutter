e21947ba4a811d32c63fb0ead25f8c8b5f95ee4c
Fiza <131092372+Fizahere@users.noreply.github.com>
2024-05-10 21:33:09 +0500

    [Friday]

--- lib/Form.dart
+++ lib/Form.dart
@@ -15,69 +15,101 @@ class _LoginFormState extends State<LoginForm> {
   TextEditingController userEmail=TextEditingController();
   TextEditingController userPassword=TextEditingController();
 
+  bool _obscureText = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor:Color(0xFFF0F0F0),
       body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 15,horizontal: 15),
+        padding: EdgeInsets.symmetric(vertical: 75,horizontal: 35),
         child: Container(
+          color: Colors.white,
           child: Form(
-              child:Column(
-                children: [
-                  TextFormField(
-                    controller: userEmail,
-                    decoration: InputDecoration(
-                      label: Text('Email**'),
-                      suffixIcon: Icon(Icons.email_outlined,color: Colors.orange,),
+              child:Padding(
+                padding: const EdgeInsets.all(30),
+                child: Column(
+                  children: [
+                Image.asset('images/insta_name.png',height: 140,width: 140,),
+                    TextFormField(
+                      controller: userEmail,
+                      decoration: InputDecoration(
+                        label: Text('Email**'),
+                        suffixIcon: Icon(Icons.email_outlined,),
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 20,),
-                  TextFormField(
-                    controller: userPassword,
-                    decoration: InputDecoration(
-                      label: Text('Password**'),
-                      suffixIcon: Icon(Icons.lock_outline,color: Colors.orange,),
+                    SizedBox(height: 20,),
+                    TextFormField(
+                      controller: userPassword,
+                      obscureText: _obscureText,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _obscureText = !_obscureText;
+                            });
+                          },
+                          icon: Icon(
+                            _obscureText
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 20,),
-                  // ElevatedButton(
-                  //   onPressed: () {
-                  //     // Navigator.push(context, MaterialPageRoute(builder: (context)=>MainScreenComponent()));
-                  //     String u_email=userEmail.text;
-                  //     String u_password=userPassword.text;
-                  //     if(u_email.isNotEmpty && u_password.isNotEmpty){
-                  //       FirebaseAuth.instance.createUserWithEmailAndPassword(email:u_email,password:u_password);
-                  //     }
-                  //   },
-                  //   child: Center(
-                  //     child: Text(
-                  //       'Sign Up',
-                  //       style: TextStyle(color: Colors.white),
-                  //     ),
-                  //   ),
-                  //   style: ButtonStyle(
-                  //     backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-                  //   ),
-                  // )
-                  ElevatedButton(
-                    onPressed: ()  {
-                      signInWithGoogle();
-                      // UserCredential? userCredential = await signInWithGoogle();
-                      // if (userCredential != null) {
-                      //   print('User signed in: ${userCredential.user!.displayName}');
-                      // } else {
-                      //   print('Error signing in with Google');
-                      // }
-                    },
-                    child: Row(
-                      children: [
-                        Icon(Icons.signal_cellular_connected_no_internet_0_bar),
-                        Text('Sign In with Google'),
-                      ],
+                    SizedBox(height: 40,),
+                            ElevatedButton(
+                onPressed: () async {
+                  String u_email = userEmail.text;
+                  String u_password = userPassword.text;
+                  try {
+                    await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                        email: u_email, password: u_password);
+                  }on FirebaseAuthException catch (e) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(e.toString()),
+                      ),
+                    );
+                  }
+                },
+                            child: Center(
+                        child: Text(
+                          'Sign Up',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                      ),
                     ),
-                  ),
+                    SizedBox(
+                      height:20
+                    ),
+                    ElevatedButton(
+                      onPressed: ()  {
+                        signInWithGoogle();
+                        // UserCredential? userCredential = await signInWithGoogle();
+                        // if (userCredential != null) {
+                        //   print('User signed in: ${userCredential.user!.displayName}');
+                        // } else {
+                        //   print('Error signing in with Google');
+                        // }
+                      },
+                      child: Center(
+                        child: Text(
+                          'Signin with google',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                      ),
+                    ),
 
-                ],
+                  ],
+                ),
               )
           ),
         ),
